b78e5a673fda5fa861d2fff58929a269d19a938c
trashb <ashleybi2005@gmail.com>
2024-02-03 18:14:54 -0500

    removed redundant function
    removed dropped_running_shoes() in player class

--- game_data.py
+++ game_data.py
@@ -235,9 +235,6 @@ class Player:
     def got_running_shoes(self):
         self.has_running_shoes = True
     
-    def dropped_running_shoes(self):
-        self.has_running_shoes = False
-    
     def check_victory(self):
         if len(self._deposited) == 3 and self.steps <= 570:
             return True
